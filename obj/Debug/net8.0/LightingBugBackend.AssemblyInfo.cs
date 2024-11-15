2872c5b73c4dbc63330615c2f5f2f9739af54a09
jourdan <jfar205@wgu.edu>
2024-11-15 16:23:02 -0600

    Updates Cintenna image1.png

--- obj/Debug/net8.0/LightingBugBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/LightingBugBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LightingBugBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7be6835f7c935fe6232c42a5b2e8b4527951dc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ae52998bb519f8f21b0fc885dd02b47698025ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("LightingBugBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LightingBugBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
